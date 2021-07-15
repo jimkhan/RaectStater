6a38cc31efebc239666a95132a914eb510b4d6ff
jimkhan <jim.khan880@gmail.com>
2021-07-15 19:24:19 +0600

    stater

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+
+import ExpenseItem from './components/ExpenseItem'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello world</h2>
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    return (
+        <div className="container">
+            <div className="date">20.10.21</div>
+            <div className="description">
+                <h2>Description</h2>
+            </div>
+            <div className="amount">
+                $3658
+            </div>
+            
+        </div>
+    )
+}
+
+export default ExpenseItem
+
+
